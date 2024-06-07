b3611170b69321852ba43abc3b19149b20a1da64
Alex <htquangg@gmail.com>
2024-06-07 11:11:36 +0700

    chore: update redis to 7

--- terraform/elasticache.tf
+++ terraform/elasticache.tf
@@ -13,10 +13,10 @@ resource "aws_elasticache_subnet_group" "main" {
 resource "aws_elasticache_cluster" "redis" {
   cluster_id           = "redis-single-node"
   engine               = "redis"
-  node_type            = "cache.t4g.micro"
+  node_type            = "cache.t3.micro"
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis6.x"
-  engine_version       = "6.2"
+  parameter_group_name = "default.redis7"
+  engine_version       = "7.1"
   port                 = 6379
   subnet_group_name    = aws_elasticache_subnet_group.main.name
   security_group_ids   = [aws_security_group.redis.id]
